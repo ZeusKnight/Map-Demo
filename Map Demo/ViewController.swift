056b1f3191558c78f8dc01566d6727744c4f3c91
Sibo <oyu-gio@hotmail.com>
2015-08-03 22:49:59 -0500

    Initial Commit

--- Map Demo/ViewController.swift
+++ Map Demo/ViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet var map: MKMapView!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        var lat:CLLocationDegrees = 41.889609;
+        var lon:CLLocationDegrees = -87.619112;
+        
+        var latDelta:CLLocationDegrees = 0.01;
+        var lonDelta:CLLocationDegrees = 0.01;
+        
+        var span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta);
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, lon);
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span);
+        map.setRegion(region, animated: true);
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
